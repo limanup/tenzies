eb5290d2d99b9c3b86111a31de162be6b4b4a1a6
Liman <liman123shen@gmail.com>
2022-07-28 13:47:37 -0400

    generate array of 10 random numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import Die from './components/Die';
+import React from "react";
+import Die from "./components/Die";
 
 function App() {
-  return (
-    <main>
-      <div className='dice-wrapper'>
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-      </div>
-    </main>
-  );
+    function allNewDice() {
+        let diceArr: number[] = [];
+
+        for (let i: number = 0; i < 10; i++) {
+            diceArr.push(Math.ceil(Math.random() * 6));
+        }
+
+        return diceArr;
+    }
+
+    console.log(allNewDice())
+
+    return (
+        <main>
+            <div className="dice-wrapper">
+                <Die value={1} />
+                <Die value={2} />
+                <Die value={3} />
+                <Die value={4} />
+                <Die value={5} />
+                <Die value={6} />
+                <Die value={1} />
+                <Die value={2} />
+                <Die value={3} />
+                <Die value={4} />
+            </div>
+        </main>
+    );
 }
 
 export default App;
