d53ad43e8d2f94a56d71faf1e4f4b9d64a2d234b
Liman <liman123shen@gmail.com>
2022-07-28 14:27:32 -0400

    add roll dice button

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,37 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Die from "./components/Die";
 
 function App() {
-    function allNewDice() {
-        let diceArr: number[] = [];
+  
+  const [diceArr, setDiceArr] = useState(allNewDice());
+  // console.log(diceArr)
+  const diceElements = diceArr.map((die) => <Die value={die} />);
+  // console.log(diceElements)
+  
+  function allNewDice() {
+      let diceArr: number[] = [];
 
-        for (let i: number = 0; i < 10; i++) {
-            diceArr.push(Math.ceil(Math.random() * 6));
-        }
+      for (let i: number = 0; i < 10; i++) {
+          diceArr.push(Math.ceil(Math.random() * 6));
+      }
 
-        return diceArr;
-    }
+      return diceArr;
+  }
 
-    const [diceArr, setDiceArr] = useState(allNewDice())
-    // console.log(diceArr)
-
-    const diceElements = diceArr.map((die) => (<Die value={die} />))
-    // console.log(diceElements)
+  function rollDice() {
+    setDiceArr(allNewDice())
+  }
 
     return (
         <main>
-            <div className="dice-wrapper">
-              {diceElements}
-            </div>
+            <div className="dice-wrapper">{diceElements}</div>
+            <button
+                className="roll-btn"
+                type="button"
+                onClick={rollDice}
+            >
+                Roll
+            </button>
         </main>
     );
 }
