74546f01a4e27e62d459b6098fc31b393d7c5264
Liman <liman123shen@gmail.com>
2022-07-30 20:27:47 -0400

    track number of rolls and time used to win, save best time to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,30 @@ interface Die {
 function App() {
     const [diceArr, setDiceArr] = useState(allNewDice());
     const [tenzies, setTenzies] = useState(false);
+    const [rollCount, setRollCount] = useState(0);
+    const [startTime, setStartTime] = useState(Date.now());
+    const [totalTimeUsed, setTotalTimeUsed] = useState(0);
+    const [localBestTime, setLocalBestTime] = useState(() =>
+        parseFloat(localStorage.getItem("tenzies") || "0")
+    );
 
     useEffect(() => {
         const winValue: number = diceArr[0].value;
         if (diceArr.every((die) => die.isHeld && die.value === winValue)) {
             setTenzies(true);
+            setTotalTimeUsed((Date.now() - startTime) / 1000);
         }
     }, [diceArr]);
+
+    useEffect(() => {
+        if (
+            totalTimeUsed > 0 &&
+            (totalTimeUsed < localBestTime || localBestTime <= 0)
+        ) {
+            setLocalBestTime(totalTimeUsed);
+            localStorage.setItem("tenzies", JSON.stringify(totalTimeUsed));
+        }
+    }, [tenzies]);
 
     const diceElements = diceArr.map((die) => (
         <Die
@@ -51,12 +68,15 @@ function App() {
         if (tenzies) {
             setDiceArr(allNewDice());
             setTenzies(false);
+            setRollCount(0);
+            setStartTime(Date.now());
         } else {
             setDiceArr((oldDice) =>
                 oldDice.map((oldDie) =>
                     oldDie.isHeld ? oldDie : generateRandomDie()
                 )
             );
+            setRollCount((prevCount) => prevCount + 1);
         }
     }
 
@@ -74,11 +94,24 @@ function App() {
         <main>
             {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
-            {tenzies && <p className="win-msg">You won!</p>}
-            {!tenzies && <p className="instructions">
-                Roll until all dice are the same. Click each die to freeze it at
-                its current value between rolls.
-            </p>}
+            {tenzies && (
+                <div className="win-msg">
+                    <h1>You won!</h1>
+                    <p>You rolled {rollCount} times. </p>
+                    <p>You used {totalTimeUsed} seconds.</p>
+                    <p>
+                        {totalTimeUsed > localBestTime
+                            ? `Best record is ${localBestTime} seconds.`
+                            : "You made a new record!"}
+                    </p>
+                </div>
+            )}
+            {!tenzies && (
+                <p className="instructions">
+                    Roll until all dice are the same. Click each die to freeze
+                    it at its current value between rolls.
+                </p>
+            )}
             <div className="dice-wrapper">{diceElements}</div>
             <button className="roll-btn" type="button" onClick={rollDice}>
                 {tenzies ? "New Game" : "Roll"}
