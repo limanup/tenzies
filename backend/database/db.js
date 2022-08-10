7f198f7f01037acd24f6cfb9f584072e27a90b7d
Liman <liman123shen@gmail.com>
2022-08-10 00:01:06 -0400

    testing db

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,5 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.port = exports.db = exports.localdb = void 0;
 require("dotenv/config");
 exports.localdb = "mongodb://localhost:27017/reactdb";
-exports.db = process.env.MONGODB_URL || '';
+// export const db = process.env.MONGODB_URL || ''
+exports.db = "mongodb+srv://admin:wQhrZ8g7vAg69PY2@tenzies.hvv6iwn.mongodb.net/leaderboard?appName=mongosh+1.5.4";
 exports.port = process.env.PORT || 4000;
--- backend/database/db.ts
+++ backend/database/db.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 
 export const localdb = "mongodb://localhost:27017/reactdb"
-export const db = process.env.MONGODB_URL || ''
+// export const db = process.env.MONGODB_URL || ''
+export const db = "mongodb+srv://admin:wQhrZ8g7vAg69PY2@tenzies.hvv6iwn.mongodb.net/leaderboard?appName=mongosh+1.5.4"
 export const port = process.env.PORT || 4000;
