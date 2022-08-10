6e9a4e8387add7f15533086bf698c1136f76499a
Liman <liman123shen@gmail.com>
2022-08-10 00:22:57 -0400

    try dotenv.config

--- backend/database/db.ts
+++ backend/database/db.ts
@@ -1,4 +1,6 @@
-import 'dotenv/config'
+// import 'dotenv/config'
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 export const localdb = "mongodb://localhost:27017/reactdb"
 export const db = process.env.MONGODB_URL || ''
