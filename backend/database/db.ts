8b2aa86154165f09602e34daa569485252436b2d
Liman <liman123shen@gmail.com>
2022-08-10 00:31:45 -0400

    removing dotenv for testing

--- backend/database/db.ts
+++ backend/database/db.ts
@@ -1,7 +1,9 @@
 // import 'dotenv/config'
-import * as dotenv from 'dotenv';
-dotenv.config()
+// import * as dotenv from 'dotenv';
+// dotenv.config()
 
 export const localdb = "mongodb://localhost:27017/reactdb"
-export const db = process.env.MONGODB_URL || ''
-export const port = process.env.PORT || 4000;
+// export const db = process.env.MONGODB_URL || ''
+// export const port = process.env.PORT || 4000;
+export const db = "mongodb://localhost:27017/reactdb"
+export const port = 4000
